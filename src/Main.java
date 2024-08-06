ec4f57a07d86efb09840c8f3fa594aaff5c33741
Alena Kilina <al.kilina2018@yandex.ru>
2024-08-06 22:42:54 +0500

    DzJava2_1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 13676;
+        int m;
+        int BonusMill;
+        {
+            m = sum / 20;
+            BonusMill = m;
+            System.out.println("Кол-во бонусных миль: " + BonusMill);
+        }
+    }
+}
